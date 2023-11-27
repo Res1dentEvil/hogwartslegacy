6f76f3492d8bc7206842c43f1cbc7da508b49568
Res1dentEvil <davidenkovano@gmail.com>
2023-11-27 19:59:46 +0200

    fix: video block

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@ const playButtonGroup = document.querySelector('.play-button-group');
 const hoverMask = document.querySelector('.hover-mask');
 const playButtonOuter = document.querySelector('.play-button-outer');
 const playButtonInner = document.querySelector('.play-button-inner');
-const videoFrame = document.querySelector('.video-frame');
+// const videoFrame = document.querySelector('.video-frame');
 const videoFrameImg = document.querySelector('.video-frame-img');
+const modalVideoShadow = document.querySelector('.modal-video-shadow');
+
+
+
 
 
 hoverMask.addEventListener("mouseover", () => {
@@ -23,11 +27,17 @@ hoverMask.addEventListener("mouseout", () => {
 })
 
 
-
 hoverMask.addEventListener("click", () => {
-    playButtonGroup.classList.add('invisible-block')
-    videoFrameImg.classList.add('invisible-block')
-    videoFrame.src = '/assets/video/Official.mp4';
-    videoFrame.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+    // playButtonGroup.classList.add('invisible-block')
+    // videoFrameImg.classList.add('invisible-block')
+    modalVideoShadow.style.opacity = '1'
+    modalVideoShadow.style.visibility = 'visible    '
+    videoFrame.src = "https://www.youtube.com/embed/BtyBjOW8sGY?si=TIjM3AbMs9yBeWu0&autoplay=1";    // videoFrame.src = '/assets/video/Official.mp4';
+    // videoFrame.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
 })
 
+modalVideoShadow.addEventListener("click", () => {
+    modalVideoShadow.style.opacity = '0'
+    modalVideoShadow.style.visibility = 'hidden'
+    videoFrame.src = "https://www.youtube.com/embed/BtyBjOW8sGY?si=TIjM3AbMs9yBeWu0";
+})
